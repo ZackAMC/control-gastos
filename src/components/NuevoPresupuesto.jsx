81c30eb8429d998eafe39b24098be697425bb6d7
ZackAMC <andresmila01@gmail.com>
2023-11-07 20:42:34 -0500

    clase uno

---
+++ src/components/NuevoPresupuesto.jsx
@@ -0,0 +1,20 @@
+const NuevoPresupuesto = () => {
+  return (
+    <div className="contenedor-presupuesto contenedor sombra">
+        <form action="" className="formulario">
+            <div className="campo">
+                <label>Definir presupuesto</label>
+                <input
+                    type="text"
+                    className="nuevo-presupuesto"
+                    placeholder="Añade tu presupuesto"
+                />
+            </div>
+
+            <input type="submit" value="Añadir" />
+        </form>
+    </div>
+  )
+}
+
+export default NuevoPresupuesto
