93e01c726b533853196cc347ade2e827144e8201
ZackAMC <andresmila01@gmail.com>
2023-11-20 20:18:08 -0500

    indicadores de presupuesto, disponible y gastado

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 function App() {
 
   const [presupuesto, setPresupuesto] = useState(0)
+  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
 
 
 
@@ -12,6 +13,8 @@ function App() {
       <Header
         presupuesto={presupuesto}
         setPresupuesto={setPresupuesto}
+        isValidPresupuesto={isValidPresupuesto}
+        setIsValidPresupuesto={setIsValidPresupuesto}
       />
     </div>
   )
