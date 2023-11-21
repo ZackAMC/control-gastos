5a7fd38a8aa2da3931e987beec7828f9241a0044
ZackAMC <andresmila01@gmail.com>
2023-11-20 19:21:46 -0500

    actualizacion 20-11

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,17 +1,43 @@
-const NuevoPresupuesto = () => {
+import { useState } from "react"
+import Mensaje from "./Mensaje"
+
+const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
+
+  const [mensaje, setMensaje] = useState('')
+
+  const handlePresupuesto = (e)=>{
+    e.preventDefault()
+
+    if (!presupuesto || presupuesto < 0) {
+      setMensaje('No es un presupuesto válido')
+
+      return
+    } 
+
+    setMensaje('')
+    console.log(presupuesto)
+
+
+  }
+
   return (
     <div className="contenedor-presupuesto contenedor sombra">
-        <form action="" className="formulario">
+        <form onSubmit={handlePresupuesto} className="formulario">
             <div className="campo">
                 <label>Definir presupuesto</label>
                 <input
-                    type="text"
+                    type="number"
                     className="nuevo-presupuesto"
                     placeholder="Añade tu presupuesto"
+                    value={presupuesto}
+                    onChange={e=> setPresupuesto(Number(e.target.value))}
                 />
             </div>
 
             <input type="submit" value="Añadir" />
+
+          { mensaje &&  <Mensaje tipo="error">{mensaje}</Mensaje>}
+
         </form>
     </div>
   )
