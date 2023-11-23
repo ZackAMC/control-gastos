58bb257928e55e54d5bdb126dcee8f5dff7233d1
ZackAMC <andresmila01@gmail.com>
2023-11-23 16:59:19 -0500

    Actualizacion 23 nov

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
-import Header from "./components/Header";
-import IconoNuevoGasto from "./img/nuevo-gasto.svg"
+import Header from "./components/Header"
 import Modal from "./components/Modal"
+import ListadoGastos from "./components/ListadoGastos"
+import { generarId } from './helpers'
+import IconoNuevoGasto from "./img/nuevo-gasto.svg"
+
 
 function App() {
 
@@ -9,14 +12,33 @@ function App() {
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
 
   const [modal, setModal] = useState(false)
+  const [animarModal, setAnimarModal] = useState(false)
+
+  const [gastos, setGastos] = useState([])
 
   const handleNuevoHasto = ()=>{
     setModal(true)
+
+    setTimeout(() => {
+      setAnimarModal(true)
+    }, 500)
+  }
+
+  const guardarGasto = gasto =>{
+    gasto.id = generarId()
+    gasto.fecha = Date.now()
+    setGastos([...gastos, gasto])
+
+    setAnimarModal(false)
+    setTimeout(() => {
+        setModal(false)    
+    }, 500)
+
   }
 
 
   return (
-    <div>
+    <div className={modal && 'fijar'}>
       <Header
         presupuesto={presupuesto}
         setPresupuesto={setPresupuesto}
@@ -25,17 +47,27 @@ function App() {
       />
 
       {isValidPresupuesto && (
-        <div className="nuevo-gasto">
-          <img
-            src={IconoNuevoGasto}
-            alt='icono nuevo gasto'
-            onClick={handleNuevoHasto}
-          />
-        </div>
+        <>
+          <main>
+            <ListadoGastos
+              gastos={gastos}
+            />
+          </main>
+          <div className="nuevo-gasto">
+            <img
+              src={IconoNuevoGasto}
+              alt='icono nuevo gasto'
+              onClick={handleNuevoHasto}
+            />
+          </div>
+        </>
       )}
 
       {modal && <Modal
                   setModal={setModal}
+                  animarModal={animarModal}
+                  setAnimarModal={setAnimarModal}
+                  guardarGasto={guardarGasto}
                 />}
       
     </div>
